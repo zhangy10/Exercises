7912f81bade26488078ce317aba696cd4836ff8a
zhangy10 <zac.lhjzyzzoo@gmail.com>
2015-08-25 17:51:43 +1000

    something updates

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+
 
 public class Test {
 
@@ -6,23 +6,27 @@ public class Test {
 //		System.out.printf("###%%d.4f###%n", 3, Math.PI);
 		
 		
-		Scanner scanner = new Scanner(System.in);
-		int n = Integer.parseInt(scanner.next());
+//		Scanner scanner = new Scanner(System.in);
+//		int n = Integer.parseInt(scanner.next());
 		// followed by next(), it should throw a nextline(), because this time will be a ""
-		scanner.nextLine();
-		String s1 = scanner.nextLine();
-		String s2 = scanner.nextLine();
-		
-		System.out.println(s1 + "-----");
-		System.out.println(s2 + "-----");
+//		scanner.nextLine();
+//		String s1 = scanner.nextLine();
+//		String s2 = scanner.nextLine();
+//		
+//		System.out.println(s1 + "-----");
+//		System.out.println(s2 + "-----");
 //		
 //		String a = "sdfsdf";
 //		System.out.printf("%10s%n", a);
+		
+		
 		
 //		System.out.printf("%e", Math.pow(10, 20));
 //		int a = Integer.MIN_VALUE;
 //		
 //		assert (a > 0): "you should keep on doing something...";
+		
+		
 		
 		
 //		int a = 60; // 111100
@@ -33,6 +37,15 @@ public class Test {
 //		System.out.println(a >> 2); // 15 001111 right smaller, means /4
 //		System.out.println(a << 2); // 240 11110000 left larger, means *4
 //		System.out.println(a << 4); // 960 1111000000 
+		
+		
+//		int result = function(5);
+//		System.out.println(result);
+		
+//		double resultFormSuar = Suar(6, "71.2", 1.2);
+//		System.exit(0);
+//		System.out.println(resultFormSuar);
+	
 	}
 	
 }
