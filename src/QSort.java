56d2da49fbcc9bf8388abbd71f743583ba2cd129
zhangy10 <zac.lhjzyzzoo@gmail.com>
2015-08-29 22:02:30 +1000

    Redesign QuickSort

--- src/QSort.java
+++ src/QSort.java
@@ -1,20 +1,106 @@
 
+/**
+ * 
+ * @author zhangy10 671205
+ *
+ *         Aug 29, 2015
+ *
+ *         QSort.java
+ * 
+ * 1. Implement for QuickSort
+ * 2. Redesign for x + y = target in a sorted array
+ * 
+ */
 public class QSort {
+
+	private int[] theArray;
+	private int arraySize;
+
+	public int[] getArray() {
+		return theArray;
+	}
+
+	public int getSize() {
+		return arraySize;
+	}
+
+	public QSort(int arraySize) {
+		this.arraySize = arraySize;
+		theArray = new int[this.arraySize];
+		generateRandomArray();
+	}
 
 	public static void main(String[] args) {
 
-		QSort sort = new QSort();
-		int[] arr = { 10, 3, 15, 1, 5, 4, 6, 2 , 12344, 22, 0, 212};
-		printArr(arr);
-		printArr(sort.quickSort(arr, 0, arr.length - 1));
+		QSort sort = new QSort(10);
+		// int[] arr = { 10, 3, 15, 1, 5, 4, 6, 2, 12344, 22, 0, 212 };
+
+		printArr(sort.getArray());
+		printArr(sort.quickSort(sort.getArray(), 0, sort.getSize() - 1));
+		Pair pair = sort.findNumber(50);
+		System.out.println(pair != null ? pair.toString() : -1);
 	}
 
-	public static void printArr(int[] arr) {
-		for (int i = 0; i <= arr.length - 1; i++) {
+	class Pair {
+		int x;
+		int y;
+
+		public String toString() {
+			return x + " " + y;
+		}
+	}
+
+	public Pair findNumber(int target) {
+		for (int i = 0; i < arraySize; i++) {
+			int subTarget = target - theArray[i];
+			if (subTarget >= 0) {
+				int result = binarySearch(subTarget, 0, arraySize - 1);
+				if (result != -1) {
+					Pair pair = new Pair();
+					pair.x = theArray[i];
+					pair.y = theArray[result];
+					return pair;
+				}
+			}
+			else {
+				break;
+			}
+		}
+
+		return null;
+	}
+
+	private int binarySearch(int target, int lo, int lh) {
+		if (lo > lh)
+			return -1;
+		int mid = (lo + lh) / 2;
+		if (theArray[mid] == target)
+			return mid;
+		else if (theArray[mid] < target)
+			return binarySearch(target, mid + 1, lh);
+		else
+			return binarySearch(target, lo, mid - 1);
+	}
+
+	public void generateRandomArray() {
+
+		for (int i = 0; i < arraySize; i++) {
+
+			// Generate a random array with values between
+			// 10 and 59
+
+			theArray[i] = (int) (Math.random() * 50) + 10;
+
+		}
+
+	}
+
+	public static void printArr(int[] theArray) {
+		for (int i = 0; i <= theArray.length - 1; i++) {
 			if (i == 0)
-				System.out.print(arr[i]);
+				System.out.print(theArray[i]);
 			else
-				System.out.print(" " + arr[i]);
+				System.out.print(" " + theArray[i]);
 		}
 		System.out.println();
 	}
--- src/Test.java
+++ src/Test.java
@@ -1,51 +1,53 @@
-
+import javax.print.attribute.standard.RequestingUserName;
 
 public class Test {
 
 	public static void main(String[] args) {
-//		System.out.printf("###%%d.4f###%n", 3, Math.PI);
-		
-		
-//		Scanner scanner = new Scanner(System.in);
-//		int n = Integer.parseInt(scanner.next());
-		// followed by next(), it should throw a nextline(), because this time will be a ""
-//		scanner.nextLine();
-//		String s1 = scanner.nextLine();
-//		String s2 = scanner.nextLine();
-//		
-//		System.out.println(s1 + "-----");
-//		System.out.println(s2 + "-----");
-//		
-//		String a = "sdfsdf";
-//		System.out.printf("%10s%n", a);
-		
-		
-		
-//		System.out.printf("%e", Math.pow(10, 20));
-//		int a = Integer.MIN_VALUE;
-//		
-//		assert (a > 0): "you should keep on doing something...";
-		
-		
-		
-		
-//		int a = 60; // 111100
-//		int b = 11; // 001011
-//		System.out.println(a | b); // 63  111111
-//		System.out.println(a & b); // 8   001000
-//		System.out.println(a ^ b); // 55  110111
-//		System.out.println(a >> 2); // 15 001111 right smaller, means /4
-//		System.out.println(a << 2); // 240 11110000 left larger, means *4
-//		System.out.println(a << 4); // 960 1111000000 
-		
-		
-//		int result = function(5);
-//		System.out.println(result);
-		
-//		double resultFormSuar = Suar(6, "71.2", 1.2);
-//		System.exit(0);
-//		System.out.println(resultFormSuar);
-	
+		// System.out.printf("###%%d.4f###%n", 3, Math.PI);
+
+		// Scanner scanner = new Scanner(System.in);
+		// int n = Integer.parseInt(scanner.next());
+		// followed by next(), it should throw a nextline(), because this time
+		// will be a ""
+		// scanner.nextLine();
+		// String s1 = scanner.nextLine();
+		// String s2 = scanner.nextLine();
+		//
+		// System.out.println(s1 + "-----");
+		// System.out.println(s2 + "-----");
+		//
+		// String a = "sdfsdf";
+		// System.out.printf("%10s%n", a);
+
+		// System.out.printf("%e", Math.pow(10, 20));
+		// int a = Integer.MIN_VALUE;
+		//
+		// assert (a > 0): "you should keep on doing something...";
+
+		// int a = 60; // 111100
+		// int b = 11; // 001011
+		// System.out.println(a | b); // 63 111111
+		// System.out.println(a & b); // 8 001000
+		// System.out.println(a ^ b); // 55 110111
+		// System.out.println(a >> 2); // 15 001111 right smaller, means /4
+		// System.out.println(a << 2); // 240 11110000 left larger, means *4
+		// System.out.println(a << 4); // 960 1111000000
+
+		// int result = function(5);
+		// System.out.println(result);
+
+		// double resultFormSuar = Suar(6, "71.2", 1.2);
+		// System.exit(0);
+		// System.out.println(resultFormSuar);
+		for (int i = 1; i < 10; i++) {
+			System.out.println(function(i));
+		}
 	}
-	
+
+	public static int function(int n) {
+		if (n == 1)
+			return 1;
+		return function(n - 1) * (2 * n - 1);
+	}
+
 }
